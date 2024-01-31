f44bd6da6cd7380bf500bdac5d98c9fb397c016b
Kamboya <kamboyah@gmail.com>
2024-01-31 15:50:04 -0800

    AdminController
    Signed-off-by: Kamboya <kamboyah@gmail.com>

--- App/app/Models/User.php
+++ App/app/Models/User.php
@@ -3,8 +3,6 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -43,17 +41,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-     /**
-     * Interact with the user's first name.
-     *
-     * @param string $value
-     * @return \Illuminate\Database\Eloquent\Casts\Attribute
-     */
-    protected function type(): Attribute
-    {
-        return new Attribute(
-            get: fn ($value)=> ["user","admin","manager"][$value],
-        );
-    }
 }
